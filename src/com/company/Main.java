1d7ca703c62844c3f1d083c22fab3b6b7f0d8c21
fvictoria <felipevictoria630@yahoo.com>
2018-11-05 11:20:16 -0500

    Initial Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
-
+import java.io.File;
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        File myFile = new File("src\\com\\company\\somePackage\\MyFile.txt");
+        Main name [] = new Main []{};
+        System.out.println(name);
+
     }
 }
